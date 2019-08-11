510515d16fd67807a04b04961eb3bfb40b4ed408
Christopher Whitehead <cawhitehead@gmail.com>
2019-08-10 21:50:45 -0400

    Set up controllers/channel

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,4 +1,12 @@
 class CallsController < ApplicationController
   def create
+    head :no_content
+    ActionCable.server.broadcast(:call_channel, call_params)
+  end
+
+  private
+
+  def call_params
+    params.permit(:call, :type, :from, :to, :sdp)
   end
 end
