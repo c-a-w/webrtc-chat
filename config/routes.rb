e25bcd966061e2b8dd022ea8c30839558bf041e7
Christopher Whitehead <cawhitehead@gmail.com>
2019-08-10 21:22:12 -0400

    Add Call ctrl and channel

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "calls#root"
+  resources :calls, only: :create
+  mount ActionCable.server, at: '/cable'
 end
