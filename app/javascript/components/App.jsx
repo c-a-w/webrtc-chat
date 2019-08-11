7255bbe031704bd951d65ea600e542386c406085
Christopher Whitehead <cawhitehead@gmail.com>
2019-08-10 21:51:43 -0400

    Add React components dir

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import VideoCall from './VideoCall';
+
+const App = () => (
+  <VideoCall />
+);
+
+export default App;
---
+++ app/javascript/components/VideoCall.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VideoCall = () => (
+  <div>Chat app</div>
+);
+
+export default VideoCall;
