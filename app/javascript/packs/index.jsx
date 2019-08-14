38537ec825730f4b00d72429fece8a2393d7516f
Christopher Whitehead <cawhitehead@gmail.com>
2019-08-14 07:17:19 -0400

    Add multiple imports of adapter.js for testing

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -6,6 +6,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import App from '../components/App';
+import adapter from 'webrtc-adapter';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
