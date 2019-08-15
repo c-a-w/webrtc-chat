82552466c53d223cf3a8506d79f2a53820970796
Christopher Whitehead <cawhitehead@gmail.com>
2019-08-14 20:11:41 -0400

    Remove some of the adapter imports

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -6,7 +6,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import App from '../components/App';
-import adapter from 'webrtc-adapter';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
