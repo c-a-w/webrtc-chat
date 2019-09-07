2bb7f03a454e5a281d71e9822a495a8082584f19
Christopher Whitehead <cawhitehead@gmail.com>
2019-09-07 19:00:24 -0400

    Get xirsys servers

--- app/javascript/components/video_util.js
+++ app/javascript/components/video_util.js
@@ -3,23 +3,10 @@ import Axios from 'axios';
 export const JOIN_CALL = 'JOIN_CALL';
 export const EXCHANGE = 'EXCHANGE';
 export const LEAVE_CALL = 'LEAVE_CALL';
-export const ice = { iceServers: [
-  {
-    urls: 'stun:stun2.l.google.com:19302'
-  }
-]};
-
-export const broadcastData = (data) => {
-  fetch('calls', {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {'content-type': 'application/json'}
-  });
-};
 function getServers() {
   Axios.get('api/v1/servers')
     .then(response => {
-      console.log(response);
+      console.log('got ice servers');
       if (response.s === 'error') {
         return null;
       }
@@ -27,7 +14,7 @@ function getServers() {
     });
 }
 
-// export const ice = getServers();
-// export const broadcastData = data => {
-//   Axios.post('calls', data);
-// };
+export const ice = getServers();
+export const broadcastData = data => {
+  Axios.post('calls', data);
+};
